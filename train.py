b39f344c7d39560a9f7d09193e994367be7cf407
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2019-09-20 23:08:06 +0300

    add detailed info to train

--- train.py
+++ train.py
@@ -1,12 +1,32 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+#======================================
+#  Breast cancer tumor classification
+# 
+#   (c) Keishi Ishihara
+#======================================
 from __future__ import print_function
-from sklearn.datasets import load_iris
+
 from sklearn.neural_network import MLPClassifier
-from gen_data import load_data
-
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from load_csv_data import load_data
 
 X_train, X_test, y_train, y_test = load_data()
-clf = MLPClassifier(solver="sgd",random_state=0,max_iter=10000)
+clf = MLPClassifier(hidden_layer_sizes=(5),
+                    activation='relu',
+                    alpha=1e-5,
+                    learning_rate_init=0.001,
+                    learning_rate='constant',
+                    solver='sgd',
+                    random_state=0,
+                    verbose=True,
+                    tol=1e-4,
+                    max_iter=500)
+
 clf.fit(X_train, y_train)
-print (clf.score(X_test, y_test))
+
+predict = clf.predict(X_test)
+
+print ('Training set score: {}'.format(clf.score(X_train, y_train)))
+print ('Test set score: {}'.format(clf.score(X_test, y_test)))
+
+ 
