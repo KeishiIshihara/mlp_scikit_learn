e5f7da0760dab9838fb4a73293085013f60c5585
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2019-09-21 15:29:46 +0300

    modified

--- train.py
+++ train.py
@@ -10,8 +10,9 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from load_csv_data import load_data
 
+
 X_train, X_test, y_train, y_test = load_data()
-clf = MLPClassifier(hidden_layer_sizes=(5),
+clf = MLPClassifier(hidden_layer_sizes=(30),
                     activation='relu',
                     alpha=1e-5,
                     learning_rate_init=0.001,
@@ -20,7 +21,7 @@ clf = MLPClassifier(hidden_layer_sizes=(5),
                     random_state=0,
                     verbose=True,
                     tol=1e-4,
-                    max_iter=500)
+                    max_iter=10000)
 
 clf.fit(X_train, y_train)
 
@@ -28,5 +29,3 @@ predict = clf.predict(X_test)
 
 print ('Training set score: {}'.format(clf.score(X_train, y_train)))
 print ('Test set score: {}'.format(clf.score(X_test, y_test)))
-
- 
